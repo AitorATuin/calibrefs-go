90344a4a2b1ce3c34fbe14ff1e5d4b3379b09374
AitorAtuin <atuin.dev@gmail.com>
2016-11-16 22:52:11 +0100

    Add flag to cmd

--- src/calibrefs/cmd/calibrefs/main.go
+++ src/calibrefs/cmd/calibrefs/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/AitorATuin/calibrefs"
 	"github.com/juju/loggo"
 )
 
+var addr = flag.String("addr", ":5640", "network address")
+
 func main() {
+	flag.Parse()
 	calibrefs.SetLogLevel(loggo.DEBUG)
 	calibrefsSrv := calibrefs.NewCalibreFS()
-	calibrefsSrv.Start(calibrefsSrv)
+	calibrefsSrv.Dotu = true
+	calibrefsSrv.Id = "calibrefs"
+	if !calibrefsSrv.Start(calibrefsSrv) {
+		fmt.Fprintf(os.Stderr, "Unable to start CalibreFS Server\n")
+		os.Exit(1)
+	}
 	calibrefsSrv.Debuglevel = 10
 	calibrefs.Logger.Infof("Starting calibrefs server")
-	err := calibrefsSrv.StartNetListener("tcp", "127.0.0.1:5640")
+	err := calibrefsSrv.StartNetListener("tcp", *addr)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err)
 	}
